9b25c698734129df2be4111e31d7c3c28a33885e
aouabira <aouabira>
2021-11-09 23:38:11 -0500

    Disable php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>  
   <body>
-      <p><?php echo "hostname is:".gethostname(); ?></p><br>
+      <p><!-- ?php echo "hostname is:".gethostname(); ?--></p><br>
       <img src='img/OIP.jpg'>
    </body>
 </html>
